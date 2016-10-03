307e1513b1a7f42be5fd8d34baebafe2d126ba87
Tamas Fabian <tamas@Tamass-MacBook-Pro.local>
2016-10-02 22:02:14 -0400

    finishing module3

--- module3/app.js
+++ module3/app.js
@@ -3,18 +3,60 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    }
+  };
+
+  return ddo;
+};
 
 NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var menu = this;
+  menu.searchTerm = "";
 
+  menu.narrowItDown = function () {
+    var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    promise.then(function (response) {
+      menu.found = response;
+    });
+  }
 
-function NarrowItDownController(service) {
+  menu.removeItem = function (index) {
+    menu.found.splice(index, 1);
+  }
+};
 
-}
-
-function MenuSearchService() {
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http) {
   var service = this;
-}
 
+  service.getMatchedMenuItems = function(searchTerm) {
+    return $http({method: "GET", url: ('https://davids-restaurant.herokuapp.com/menu_items.json')})
+    .then(function (result) {
+      // process result and only keep items that match
+      var menuItems = result.data.menu_items;
+      var foundItems = [];
+      if (searchTerm) {
+        for (var i = 0; i < menuItems.length; i++) {
+          if (menuItems[i].description.indexOf(searchTerm) !== -1) {
+            foundItems.push(menuItems[i]);
+          }
+        }
+      }
 
-})();
+      // return processed items
+      return foundItems;
+    });
+  };
+};
+
+})()
